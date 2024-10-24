234d63d20fdc13d80dceef5828526030fd1cc571
JsLth <jslth@outlook.com>
2024-10-25 01:19:47 +0200

    handle import and start processes differently and make log parsing more robust

--- R/run_photon.R
+++ R/run_photon.R
@@ -1,19 +1,53 @@
-run_photon <- function(self, private, java_opts = NULL, photon_opts = NULL) {
+run_photon <- function(self,
+                       private,
+                       mode,
+                       timeout = 60,
+                       java_opts = NULL,
+                       photon_opts = NULL) {
   quiet <- private$quiet
   version <- private$version
   path <- self$path
 
-  exec <- get_photon_executable(path, version)
+  exec <- get_photon_executable(path, version, private$opensearch)
   path <- normalizePath(path, winslash = "/")
   args <- c(java_opts, "-jar", exec, photon_opts)
 
-  proc <- processx::process$new(
+  switch(
+    mode,
+    import = run_import(self, private, args, timeout, quiet),
+    start = run_start(self, private, args, timeout, quiet)
+  )
+}
+
+
+run_import <- function(self, private, args, timeout = 60, quiet = FALSE) {
+  run(
     "java",
     args = args,
     stdout = "|",
     stderr = "|",
     echo_cmd = globally_enabled("photon_debug", FALSE),
-    wd = path
+    wd = self$path,
+    timeout = timeout,
+    error_on_status = TRUE,
+    stdout_callback = function(newout, proc) {
+      if (nzchar(newout) && !quiet) {
+        cli::cli_verbatim(newout)
+        handle_log_conditions(newout)
+      }
+    }
+  )
+}
+
+
+run_start <- function(self, private, args, timeout = 60, quiet = FALSE) {
+  proc <- process$new(
+    "java",
+    args = args,
+    stdout = "|",
+    stderr = "|",
+    echo_cmd = globally_enabled("photon_debug", FALSE),
+    wd = self$path
   )
 
   if (globally_enabled("photon_movers")) {
@@ -21,11 +55,17 @@ run_photon <- function(self, private, java_opts = NULL, photon_opts = NULL) {
       msg = "Starting photon...",
       msg_done = "Photon is now running.",
       msg_failed = "Photon could not be started.",
-      spinner =
+      spinner = TRUE
     )
   }
 
-  while (!photon_ready(self, private)) {
+  start <- Sys.time()
+  while (!photon_ready(self, private) && proc$is_alive()) {
+    diff <- Sys.time() - start
+    if (diff > timeout) {
+      ph_stop("Photon setup timeout reached.")
+    }
+
     out <- proc$read_output()
     if (nzchar(out) && !quiet) {
       cli::cli_verbatim(out)
@@ -34,7 +74,7 @@ run_photon <- function(self, private, java_opts = NULL, photon_opts = NULL) {
 
     err <- proc$read_error()
     if (nzchar(err)) {
-      cli::cli_abort(err) # nocov
+      ph_stop(err) # nocov
     }
   }
 
@@ -64,44 +104,59 @@ handle_log_conditions <- function(out) {
   warnings <- out[grepl("WARN", out, fixed = TRUE)]
   if (length(warnings) && globally_enabled("photon_setup_warn")) {
     msg <- parse_log_line(out)$msg
-    cli::cli_warn(trimws(msg))
+    if (!is.null(msg)) {
+      cli::cli_warn(trimws(msg))
+    }
   }
 
-  exceptions <- out[grepl("exception", out, ignore.case = TRUE)]
-  if (length(exceptions)) {
-    msg <- parse_log_line(out)$msg
-    cli::cli_abort(trimws(msg))
-  }
+  # exceptions <- out[grepl("exception", out, ignore.case = TRUE)]
+  # if (length(exceptions)) {
+  #   msg <- parse_log_line(out)$msg
+  #   ph_stop(trimws(msg))
+  # }
 
   if (startsWith(out, "Usage")) {
-    cli::cli_abort("Process returned an error.")
+    ph_stop("Process returned an error.")
   }
 }
 
 
 parse_log_line <- function(line) {
-  line <- strcapture(
-    "\\[(.+)\\]\\[(.+)\\]\\[([a-zA-Z. ]+)\\](.+)",
+  # photon logs have to different forms:
+  # 1. timestamp [main] INFO trace - message
+  # 2. [timestamp] [INFO] [stream] message
+  # if 1. matches nothing, try 2.
+  parsed <- utils::strcapture(
+    "^(.+) \\[(.+)\\] (INFO|WARN) (.+) - (.+)$",
     line,
-    proto = list(
-      ts = character(),
-      type = character(),
-      stream = character(),
-      msg = character()
-    )
+    proto = data.frame(ts = "", src = "", type = "", trace = "", msg = "")
   )
+
+  if (all(is.na(parsed))) {
+    parsed <- utils::strcapture(
+      "\\[(.+)\\]\\[(.+)\\]\\[([a-zA-Z. ]+)\\](.+)",
+      line,
+      proto = list(ts = "", type = "", src = "", msg = "")
+    )
+  }
+
+  if (all(is.na(parsed))) {
+    parsed <- NULL
+  }
+
+  parsed
 }
 
 
-get_photon_executable <- function(path, version) {
-  if (has_opensearch_jar(path)) {
+get_photon_executable <- function(path, version, opensearch) {
+  if (opensearch) {
     file <- sprintf("photon-opensearch-%s.jar", version)
   } else {
     file <- sprintf("photon-%s.jar", version)
   }
 
   if (!file.exists(file.path(path, file))) {
-    cli::cli_abort("Photon jar {.val {file}} could not be found in the given path.")
+    ph_stop("Photon jar {.val {file}} could not be found in the given path.")
   }
 
   file
