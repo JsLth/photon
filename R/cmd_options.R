b5bd90f98119792541c8137cf724f3122271672b
JsLth <jslth@outlook.com>
2024-10-26 02:57:03 +0200

    enhance cmd_options() examples

--- R/cmd_options.R
+++ R/cmd_options.R
@@ -25,13 +25,13 @@
 #'
 #' @examples
 #' # converts R parameters to CMD options
-#' opts <- cmd_options(n = 1, w = 5)
-#' run("ping", c(opts, "127.0.0.1"), error_on_status = FALSE, echo_cmd = TRUE)
+#' opts <- cmd_options(n = 1, w = 5, "127.0.0.1")
+#' run("ping", args = opts, error_on_status = FALSE, echo_cmd = TRUE)
 #'
 #' if (nzchar(Sys.which("docker"))) {
 #' # sometimes, it is necessary to use double hyphens
-#' opts <- cmd_options(all = TRUE, format = "json", use_double_hyphens = TRUE)
-#' run("docker", c("ps", opts), error_on_status = FALSE, echo_cmd = TRUE)
+#' opts <- cmd_options("ps", all = TRUE, format = "json", use_double_hyphens = TRUE)
+#' run("docker", args = opts, error_on_status = FALSE, echo_cmd = TRUE)
 #' }
 #'
 #' if (FALSE) {
